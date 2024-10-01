af4a9ec58a10f33d3b859b621f58f09e61051eaf
paddington22 <pkorolev23@gmail.com>
2024-10-01 14:26:49 +0300

    feat: task4 with exception (completed)

---
+++ task4/task4.py
@@ -0,0 +1,26 @@
+file_path = input()
+
+
+def solution(file_path):
+    result = 0
+    with open(file_path, 'r', encoding='utf-8') as in_file:
+        data = in_file.readlines()
+    data = sorted(int(i.strip()) for i in data)
+    length = len(data)
+
+    if length % 2:
+        median = data[length // 2]
+    else:
+        median = round((data[length // 2] + data[length // 2 - 1]) / 2)
+
+    for num in data:
+        result += abs(median - num)
+    return result
+
+
+if __name__ == '__main__':
+    try:
+        result = solution(file_path)
+        print(result)
+    except Exception as err:
+        print(err)
