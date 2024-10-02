7defeb286d09ef48f37600426529df08f494dfa7
paddington22 <pkorolev23@gmail.com>
2024-10-02 17:16:16 +0300

    feat: added args of cmd, and check radius

--- task2/task2.py
+++ task2/task2.py
@@ -1,11 +1,20 @@
-circle_file = input()    #change for input()
-dots_file = input()    #change for input()
+import argparse
+
+parser = argparse.ArgumentParser(description='Script arguments')
+parser.add_argument('circle_file', help='Path to file with circle parametrs')
+parser.add_argument('dots_file', help='Path to file with dots coordinates')
+
+args = parser.parse_args()
+circle_file = args.circle_file
+dots_file = args.dots_file
 
 
 def solution(circle_file, dots_file):
     with open(circle_file, 'r', encoding='utf-8') as circle_data:
         x, y = map(int, circle_data.readline().strip().split())
         radius = int(circle_data.readline())
+        if radius < 1:
+            raise ValueError('Radius must be > 0')
 
     with open(dots_file, 'r', encoding='utf-8') as dots_data:
         for dots in dots_data:
